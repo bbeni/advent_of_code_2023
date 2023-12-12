31dfd1770944f263fd5887ea524d2dde39991e6e
Benjamin Froelich <bbenif@gmail.com>
2023-12-12 23:34:54 +0100

    finally part 2 uff

--- py_days/prob_12_2.py
+++ py_days/prob_12_2.py
@@ -2,43 +2,39 @@ import math
 
 def linear_count(seq, block):
     s = 0
-    for i in range(len(seq)-block):
-        if not ('.' in seq[i:i+block]):
+    for i in range(0, len(seq)-block+1):
+        if (not '.' in seq[i:i+block]) and (not '#' in seq[:i]+seq[i+block:]) :
             s += 1
     return s
 
 def partition_count(seq, counts):
-    print(seq, counts)
+    if sum(counts) + len(counts) - 1 > len(seq):
+        return 0
     if counts == []:
         return 1
     if len(counts) == 1:
-        return linear_count(seq, counts[0])
-    if sum(counts) + len(counts) - 1 > len(seq):
-        return 0
+        l = linear_count(seq, counts[0])
+        return l
+    if len(counts) == 2:
+        first_block = counts[0]
+        second_block = counts[1]
+        t = 0
+        for i in range(0, len(seq)-first_block-second_block):
+            if (not '.' in seq[i:i+first_block]) and (not '#' in seq[:i]+seq[i+first_block]):
+                t += linear_count(seq[i+first_block+1:], second_block)
+        return t
 
-    middle = math.ceil(len(seq)/2)
+    mid = math.floor(len(counts)/2)
+    mid_block = counts[mid]
+    start = len(counts[:mid]) + sum(counts[:mid])
+    end = len(seq) - len(counts[mid+1:]) - sum(counts[mid+1:]) - mid_block +1
+
     s = 0
-    for i in range(1, len(counts)):
-        l = partition_count(seq[:middle], counts[:i])
-        r = partition_count(seq[middle:], counts[i:])
-
-        # check for overlap case from left
-        x = counts[i-1]
-        for offset in range(1, x):
-            if (not '.' in seq[middle-offset:middle-offset+x]):
-                lx = partition_count(seq[:middle-offset], counts[:i-1])
-                rx = partition_count(seq[middle-offset+x:], counts[i:])
-                #s += lx*rx
-
-        # check for overlap case from right
-        x = counts[i]
-        for offset in range(1, x):
-            if (not '.' in seq[middle-offset:middle-offset+x]):
-                lx = partition_count(seq[:middle-offset], counts[:i])
-                rx = partition_count(seq[middle-offset+x:], counts[i+1:])
-                #s += lx*rx
-
-        s += l*r
+    for i in range(start, end):
+        if not( '.' in seq[i:i+mid_block]) and not ('#' in seq[i-1]+seq[i+mid_block]):
+            left = partition_count(seq[:i-1], counts[:mid])
+            right = partition_count(seq[i+mid_block+1:], counts[mid+1:])
+            s += left*right
     return s
 
 
@@ -46,15 +42,14 @@ with open(0) as f:
     lines = f.readlines()
 
 s = 0
-for i, l in enumerate(lines[-1:]):
+for i, l in enumerate(lines):
     a, b = l.split(' ')
     b = b.strip()
     b = (5*(b+','))[:-1]
     counts = list(map(int, b.strip().split(',')))
     seq = (5*(a+'?'))[:-1]
-    print('starting', seq, counts)
     t = partition_count(seq, counts)
-    print('got count:', t)
+    #print('got count:', t)
     s += t
 
 print(s)
